2767cfe2e3b05cdf6cdaf0c6adf0d20ea6e5d939
SergTron <mr.trofan@gmail.com>
2018-09-27 17:01:57 +0200

    X and Z limitations added in Generator. iteration step added like 0.002. added subCycleNumber variable

--- src/sample/Generator.java
+++ src/sample/Generator.java
@@ -1,16 +1,33 @@
 package sample;
 
 public class Generator {
+    CartesianCoordinates cartesianCoordinates = new CartesianCoordinates();
+    // One oscilator cycle (whole circle) has 4 sub-cycles
+    // each of them describes x and z coordinates increasing and decreasing:
+    // in 1 subCycle x - up, z - up;
+    // in 2 subCycle x - up, z - down;
+    // in 3 subCycle x - down, z - down;
+    // in 4 subCycle x - down, z - up;
+    int subCycleNumber = 1;
+    float iterationStep = 0.002f;
+
+    double LimiterMaxX = 10.0;
+    double LimiterMinX = -10.0;
+    double LimiterMaxZ = -116.0;
+    double LimiterMinZ = -127.0;
+
     Generator(){
+        cartesianCoordinates.x = 0;
+        cartesianCoordinates.y = 86.5;
+        cartesianCoordinates.z = 0;
     }
 
 
-    //Now any I in parameters!
-    public CartesianCoordinates createCoordinates(CartesianCoordinates cartesianCoordinates,int i)
-    {
-      cartesianCoordinates.x = 1;
+    public CartesianCoordinates setCoordinates() {
+        if
+      cartesianCoordinates.x += iterationStep;
       cartesianCoordinates.y = 86.5;
-      cartesianCoordinates.z = -121.5;
+      cartesianCoordinates.z += iterationStep ;
         return cartesianCoordinates;
     }
 }
